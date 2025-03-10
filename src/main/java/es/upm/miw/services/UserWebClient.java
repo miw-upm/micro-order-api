905c17b510a0c793589e150f3a0963ae99bc83f3
JBernal <j.bernal@upm.es>
2025-03-10 15:11:18 +0100

    fixed gateway

--- src/main/java/es/upm/miw/services/UserWebClient.java
+++ src/main/java/es/upm/miw/services/UserWebClient.java
@@ -5,11 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = UserWebClient.API_USER)
+@FeignClient(name = UserWebClient.MICRO_USER_API)
 public interface UserWebClient {
 
     String USERS_IDENTITY_ID = "/users/{identity}";
-    String API_USER = "micro-api-user";
+    String MICRO_USER_API = "micro-user-api";
 
     @GetMapping(USERS_IDENTITY_ID)
     User readUserByIdentity(@PathVariable String identity);
